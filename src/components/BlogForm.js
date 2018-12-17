b858166b53097fdb7164988b5c9a57278a1848b3
Matt Westover <matthew.westover@gmail.com>
2018-12-17 14:05:18 -0700

    add basic blog form

--- src/components/BlogForm.js
+++ src/components/BlogForm.js
@@ -1,10 +1,40 @@
 import React from 'react';
+import { connect, } from 'react-redux';
 
-const BlogForm = () => {
-  return (
-    <div>
-    </div>
-  )
+class BlogForm extends React.Component {
+  state = { title: "", body: "", };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { dispatch } = this.props;
+    const { title, body, } = this.state;
+    const blog = { title, body };
+    dispatch({ type: 'ADD_BLOG', blog })
+    this.setState({ title: "", body: "", })
+  }
+
+  handleChange = ({ target: { name, value }}) => {
+    this.setState({ [name]: value, });
+  }
+
+  render() {
+    const { title, body, } = this.state;
+    return(
+      <div>
+        <h3>Add a Blog</h3>
+        <form onSubmit={this.handleSubmit}>
+          Blog Title: <br />
+          <input name="title" value={title} onChange={this.handleChange} />
+          <br />
+          Blog Body: <br />
+          <textarea name="body" value={body} onChange={this.handleChange} />
+          <br />
+          <br />
+          <input type="submit" value="submit" />
+        </form>
+      </div>
+    )
+  }
 }
 
-export default BlogForm;
+export default connect()(BlogForm);
